b6578d36949745df6932707b66df3035de5a665d
Kuroosh <ocelotetv@gmail.com>
2024-04-24 23:11:35 +0200

    Init

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,9 @@
 export default {
 	ownerBot: '436502093808008@s.whatsapp.net',
 	prefix: '/',
-	authKey: '5176079407:AAF4dropoId1Gx88ANkYIrpBWFdzRnvAimE',
-	openAiKey: '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',
+	authKey: '',
+	openAiKey: 'YOUR_KEY',
 	isDebug: false,
-	//assetPath: './assets/',
-	//globalPath: './',
-	assetPath: 'C:\\Users\\kuroosh\\OneDrive\\Documents\\VenoX-WA\\assets\\',
-	globalPath: 'C:\\Users\\kuroosh\\OneDrive\\Documents\\VenoX-WA\\',
-	//
-	//assetPath: '../VenoX-WA/assets/',
-	//globalPath: '../VenoX-WA/'
-	//globalPath: '/home/venox/assets/'
+	assetPath: 'C:\\Users\\user\\OneDrive\\Documents\\VenoX-WA\\assets\\',
+	globalPath: 'C:\\Users\\user\\OneDrive\\Documents\\VenoX-WA\\'
 };
